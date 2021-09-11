e753c1d63be892599255fc45898f3bc82bf58814
Eloagbawe <eloagbawe@gmail.com>
2021-09-11 23:42:24 +0100

    http client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import {Post} from './post';
 import { Observable } from 'rxjs';
 @Component({
@@ -11,12 +11,20 @@ export class AppComponent {
   title = 'Elo Agbawe Idiodi, Welcome to Angular';
   readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
 
-  posts: any;
+  //posts: Observable<Post[]> | undefined;
+
+  posts: Observable<any> | undefined;
 
   constructor(private http : HttpClient){}
 
   getPosts(){
-    this.posts = this.http.get(this.ROOT_URL + '/posts');
+    //this.posts = this.http.get<Post[]>(this.ROOT_URL + '/posts');
+
+    //this.posts = this.http.get(this.ROOT_URL + '/posts?id=1');
+
+    let params = new HttpParams().set('userId', '1');
+
+    this.posts = this.http.get(this.ROOT_URL + '/posts',{params});
   }
 
 }
