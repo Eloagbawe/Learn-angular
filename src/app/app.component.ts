f06e144f9e08a751ad1b0f8f54ef5e7e726bf8bc
Eloagbawe <eloagbawe@gmail.com>
2021-09-12 22:48:25 +0100

    http client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import {Post} from './post';
 import { Observable } from 'rxjs';
 @Component({
@@ -14,6 +14,7 @@ export class AppComponent {
   //posts: Observable<Post[]> | undefined;
 
   posts: Observable<any> | undefined;
+  newPost: Observable<any> | undefined;
 
   constructor(private http : HttpClient){}
 
@@ -22,9 +23,24 @@ export class AppComponent {
 
     //this.posts = this.http.get(this.ROOT_URL + '/posts?id=1');
 
-    let params = new HttpParams().set('userId', '1');
+    //let params = new HttpParams().set('userId', '1');
 
-    this.posts = this.http.get(this.ROOT_URL + '/posts',{params});
+    let headers = new HttpHeaders().set('Authorization', 'auth-token');
+
+    //this.posts = this.http.get(this.ROOT_URL + '/posts',{params});
+
+    this.posts = this.http.get(this.ROOT_URL + '/posts',{headers});
+  }
+
+  createPost(){
+    const data: Post = {
+      id: null,
+      userId: 23,
+      title: 'My New Post',
+      body: 'Hello World'
+    }
+    
+    this.newPost = this.http.post(this.ROOT_URL + '/posts', data);
   }
 
 }
