d8a17f6ee1df3eae04220e02cdc630ccd3c74a57
Eloagbawe <eloagbawe@gmail.com>
2021-09-29 22:56:38 +0100

    notes app

--- test/src/app/app.component.ts
+++ test/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Elo Agbawe Idiodi, Welcome to Angular';
