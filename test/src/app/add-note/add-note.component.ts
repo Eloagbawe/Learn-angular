ec39c267484438f5291211ce6f90d0507e85a383
Eloagbawe <eloagbawe@gmail.com>
2021-10-03 23:20:16 +0100

    minor change

--- test/src/app/add-note/add-note.component.ts
+++ test/src/app/add-note/add-note.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddNoteComponent implements OnInit {
 
+  title:string | undefined
+  text:string | undefined
+
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(){
+
+    const newNote = {
+      title: this.title,
+      text: this.text,
+    }
+  
+
+  }
+  
 }
