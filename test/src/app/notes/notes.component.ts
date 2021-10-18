2657345fa955b4b6019173c3714de7860de64f39
Eloagbawe <eloagbawe@gmail.com>
2021-10-18 21:35:40 +0100

    note app completed

--- test/src/app/notes/notes.component.ts
+++ test/src/app/notes/notes.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import {map, catchError, retry} from 'rxjs/operators';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Note } from '../note'
 import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-notes',
@@ -16,31 +16,131 @@ export class NotesComponent implements OnInit {
 
   notes: any = [];
 
+  title: string | undefined;
+  note: string | undefined;
+
+  updatedTitle : string | undefined;
+  updatedNote : string | undefined;
+  
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+  
+
+
+
+  @ViewChild('addCheck')
+  addCheck!: ElementRef;
+
+  @ViewChild('editCheck')
+  editCheck!: ElementRef;
+
+  //ngAfterViewInit() {}
+
+  noteToUpdate: Note = {
+    title: '',
+    note: '',
+    important: false
+  }
+
+
   constructor(private http: HttpClient, private router : Router) { }
 
-  getNotes(){
+
+  //Create/Post
+
+  add(){
+
+    const newNote = {
+      title: this.title,
+      note: this.note,
+      important: this.addCheck.nativeElement.checked
+    }
+
+    return this.http.post(this.apiUrl, newNote, this.httpOptions)
+  }
+
+  addNote(){
+    this.add().subscribe((note) => {console.log(note)})
+
+   
+    alert("Note Added Successfully!")
+    this.getNotes()
+    
+  }
+
+
+
+
+  //Read/Get
+  get(){
     //let headers = new HttpHeaders().set('Authorization', 'auth-token');
     return this.http.get(this.apiUrl)
   }
 
+  getNotes(){
+    this.get().subscribe((notes) => (this.notes = notes))
+  }
+
+
+  
+
+
+  //Update
+
+  triggerEdit(note : Note){
+    
+
+    this.noteToUpdate = {
+      id: note.id,
+      title: note.title,
+      note: note.note,
+      important: note.important
+    }
+    this.editCheck.nativeElement.checked = note.important
+  }
+
+
+
+
+  edit(){
+
+    const updatedNote = {
+      id: this.noteToUpdate.id,
+      title: this.updatedTitle,
+      note: this.updatedNote,
+      important: this.editCheck.nativeElement.checked
+
+    }
+    return this.http.patch(`${this.apiUrl}/${updatedNote.id}`, updatedNote, this.httpOptions)
+  }
+
+
+  editNote(){
+    this.edit().subscribe((note) => {
+      console.log(note)
+    })
+   
+    alert("Note Successfully Edited!")
+    this.getNotes()
+    
+  }
+
+
+
+  //Delete
   delete(note : Note){
     return this.http.delete(`${this.apiUrl}/${note.id}`)
   }
 
-  edit(note : Note){
-    return this.http.put(`${this.apiUrl}/${note.id}`, note )
-  }
-
-  editNote(note : Note){
-    this.edit(note).subscribe()
-  }
   deleteNote(note : Note){
     this.delete(note).subscribe(() => {
       this.notes = this.notes.filter((n: Note) => n.id !== note.id)
-
+    
       
       alert("Note Successfully Deleted!")
-      this.router.navigate(["/notes"])
       
     }
     )
@@ -48,7 +148,7 @@ export class NotesComponent implements OnInit {
 
   
   ngOnInit(): void {
-    this.getNotes().subscribe((notes) => (this.notes = notes))
+    this.getNotes()
     //this.getNotes().subscribe((notes) => (console.log(notes)))
   }
 
