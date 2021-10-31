8d50205f05e3f29690a94265e0cd73394128302d
Eloagbawe <eloagbawe@gmail.com>
2021-10-31 22:29:10 +0100

    minor fix

--- ngrx-app/src/app/app.module.ts
+++ ngrx-app/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { StoreModule } from '@ngrx/store';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 
 
@@ -28,7 +30,12 @@ import { GalleryEffect } from './components/gallery/gallery.effect';
     HttpClientModule,
     FormsModule,
     StoreModule.forRoot({ gallery: galleryReducer }),
-    EffectsModule.forRoot([GalleryEffect])
+    EffectsModule.forRoot([GalleryEffect]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
 
   ],
   providers: [GalleryService],
