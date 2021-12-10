25cf4601685d49e6909c3717799122332f85f904
Eloagbawe <eloagbawe@gmail.com>
2021-12-10 23:36:24 +0100

    simple ngrx setup

---
+++ ngrx-app/src/app/store/photo.reducer.ts
@@ -0,0 +1,12 @@
+import {createReducer, on} from '@ngrx/store';
+import {dislikePhoto, likePhoto} from './photo.action';
+
+export type PhotoState = number;
+
+const initialState: PhotoState = 0;
+
+export const photoReducer = createReducer(
+  initialState,
+  on(likePhoto, state => state + 1),
+  on(dislikePhoto, state => state - 1)
+);
