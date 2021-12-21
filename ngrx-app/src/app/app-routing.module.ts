b6a9a7388e6b462963ef474b943358a7a9a48a7f
Eloagbawe <eloagbawe@gmail.com>
2021-12-21 23:49:18 +0100

    minor fix

--- ngrx-app/src/app/app-routing.module.ts
+++ ngrx-app/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CounterComponent } from './components/counter/counter.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component:CounterComponent
+  },
+
+  {
+    path: "gallery",
+    component: GalleryComponent
+  },
+  {
+    path: "counter",
+    component: CounterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
